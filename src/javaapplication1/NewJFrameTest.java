f77a060519aac9c517bdddc27d2222401d3ef3f0
Gavin Eschtruth <gavin.eschtruth@gmail.com>
2022-12-07 23:55:00 -0500

    Next turn is done just needs to be polished

---
+++ src/javaapplication1/NewJFrameTest.java
@@ -0,0 +1,12 @@
+package javaapplication1;
+
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewJFrameTest {
+
+    @Test
+    void main() {
+    }
+}
