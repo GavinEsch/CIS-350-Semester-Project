b11154999d8d3fa2f6b458407b924056cad24c1c
Gavin Eschtruth <gavin.eschtruth@gmail.com>
2022-12-07 19:31:12 -0500

    added AI class

---
+++ src/javaapplication1/AI.java
@@ -0,0 +1,23 @@
+package javaapplication1;
+
+public class AI {
+    public void nextTurn() {
+
+
+    }
+
+    public void bidSubmit() {
+
+
+    }
+
+    public void challengePlayer() {
+
+
+    }
+
+    public void easyAI() {
+
+
+    }
+}
